9eb09aac3df61f9d28a757191b0e11398ac8ae3d
chaks03 <chakshu@samagragovernance.in>
2021-07-01 19:04:22 +0530

    Fix: issues with dao

--- src/main/java/com/uci/dao/Application.java
+++ src/main/java/com/uci/dao/Application.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
@@ -15,6 +16,7 @@ import java.util.UUID;
  * @author chakshu
  */
 @Slf4j
+@ConfigurationProperties("dao-application.properties")
 @SpringBootApplication(scanBasePackages = "com.uci.dao")
 public class Application implements CommandLineRunner {
 
