9552fe492d4d2bf10b927130343ed7138e1c40d0
pankajjangid05 <pankaj.jangid@aurigait.com>
2023-06-20 14:49:17 +0530

    Change cassandra configuration & Build Version Change

--- src/main/java/com/uci/dao/config/CassandraConfig.java
+++ src/main/java/com/uci/dao/config/CassandraConfig.java
@@ -1,22 +1,15 @@
 package com.uci.dao.config;
 
-import com.datastax.driver.core.Cluster;
-import com.datastax.driver.core.QueryLogger;
-import com.datastax.oss.driver.api.core.CqlSessionBuilder;
 import com.datastax.oss.driver.api.core.config.DefaultDriverOption;
-import com.datastax.oss.driver.api.core.config.DriverConfigLoader;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.cassandra.config.AbstractReactiveCassandraConfiguration;
+import org.springframework.data.cassandra.config.DriverConfigLoaderBuilderConfigurer;
 import org.springframework.data.cassandra.config.SchemaAction;
-import org.springframework.data.cassandra.config.SessionBuilderConfigurer;
 import org.springframework.data.cassandra.core.cql.keyspace.CreateKeyspaceSpecification;
-import org.springframework.data.cassandra.core.cql.keyspace.DropKeyspaceSpecification;
 import org.springframework.data.cassandra.core.cql.keyspace.KeyspaceOption;
 import org.springframework.data.cassandra.repository.config.EnableReactiveCassandraRepositories;
 
-import java.time.Duration;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -84,13 +77,24 @@ public class CassandraConfig extends AbstractReactiveCassandraConfiguration {
 //            @Override
 //            public CqlSessionBuilder configure(CqlSessionBuilder cqlSessionBuilder) {
 //                return cqlSessionBuilder
-//                        .withConfigLoader(DriverConfigLoader.programmaticBuilder().withDuration(DefaultDriverOption.REQUEST_TIMEOUT, Duration.ofMillis(15000)).build());
+//                        .withConfigLoader(DriverConfigLoader.programmaticBuilder().withDuration(DefaultDriverOption.REQUEST_TIMEOUT, Duration.ofMillis(1200000)).build());
 //            }
 //        };
 //    }
 
+    @Override
+    protected DriverConfigLoaderBuilderConfigurer getDriverConfigLoaderBuilderConfigurer() {
+        return config ->
+                config.withString(DefaultDriverOption.METADATA_SCHEMA_REQUEST_TIMEOUT, "30s")
+                        .withString(DefaultDriverOption.CONTROL_CONNECTION_TIMEOUT, "10s")
+                        .withString(DefaultDriverOption.REQUEST_TIMEOUT, "30s")
+//                        .withString(DefaultDriverOption.REQUEST_CONSISTENCY, "ALL")
+                        .build();
+    }
+
     /**
      * Get list of scripts run on startup
+     *
      * @return
      */
     @Override
@@ -101,18 +105,18 @@ public class CassandraConfig extends AbstractReactiveCassandraConfiguration {
             if (migrationCount > 0) {
                 count = migrationCount;
             }
-            if(migrationCount > all.size()) {
+            if (migrationCount > all.size()) {
                 count = all.size();
             }
-            System.out.println("Count: "+count+", migrationCount: "+migrationCount);
-        } catch(NumberFormatException ex){
+            System.out.println("Count: " + count + ", migrationCount: " + migrationCount);
+        } catch (NumberFormatException ex) {
             System.out.println("NumberFormatException: " + ex.getMessage());
-        } catch(Exception ex){
+        } catch (Exception ex) {
             System.out.println("Exception: " + ex.getMessage());
         }
 
         List<String> scripts = new ArrayList<>();
-        for(int i=(count); i<all.size(); i++) {
+        for (int i = (count); i < all.size(); i++) {
             scripts.add(all.get(i));
         }
 
@@ -121,6 +125,7 @@ public class CassandraConfig extends AbstractReactiveCassandraConfiguration {
 
     /**
      * List of migration scripts
+     *
      * @return
      */
     protected List<String> getMigrationScripts() {
